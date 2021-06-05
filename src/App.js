e2aded43c5fba7879625fb5738f0a32109cde632
Maxieprodmoore <binglinho@gmail.com>
2021-06-05 18:40:05 +0800

    modified app.js for upload

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App {
   constructor(){
     this.name = "Creahaven"
     this.version = "1.0.0"
-    this.apiBase = 'http://localhost:3000'
+    this.apiBase = 'https://blho-creahaven-backend.herokuapp.com/'
     this.rootEl = document.getElementById("root")
     this.version = "1.0.0"
   }
