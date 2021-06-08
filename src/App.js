c0e90dbe51e71e951b52219babf57eb36c58d579
Maxieprodmoore <binglinho@gmail.com>
2021-06-08 11:13:55 +0800

    edited html links

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App {
   constructor(){
     this.name = "Creahaven"
     this.version = "1.0.0"
-    this.apiBase = "blho-creahaven-backend.herokuapp.com"
+    this.apiBase = `blho-creahaven-backend.herokuapp.com`
     this.rootEl = document.getElementById("root")
     this.version = "1.0.0"
   }
